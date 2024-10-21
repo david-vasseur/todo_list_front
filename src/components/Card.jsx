a45191fc7ed1aa8b49c45a2fc3fb1aa9d6feffc8
David <p5y4@laposte.net>
2024-10-21 17:09:07 +0200

    changing cards appearence

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -22,7 +22,7 @@ function Card({ item, onDelete, onUpdate, isModified, setIsModified, updateValue
     }
 
   return (
-    <Link to={toLink(item)} className="pl-4 pr-4 p-2 bg-[#8e05c2] text-[#dbd8e3] rounded-3xl w-[80vw] md:w-[60vw] lg:w-[30vw] font-extrabold border-2 border-slate-900 flex gap-12 justify-between transition-transform duration-300 ease-in-out items-center shadow-md shadow-black hover:translate-x-2 will-change-transform">
+    <Link to={toLink(item)} className="pl-4 pr-4 p-2 bg-[#8e05c2] text-[#dbd8e3] rounded-3xl w-[80vw] md:w-[60vw] lg:w-[30vw] font-extrabold border-2 border-slate-900 flex gap-12 justify-between transition-transform duration-300 ease-in-out items-center shadow-md shadow-black sm:hover:translate-x-2 will-change-transform">
       {isModified === item.id ? (
         <div className="flex gap-2 justify-between w-[100%]">
             <input
@@ -37,7 +37,7 @@ function Card({ item, onDelete, onUpdate, isModified, setIsModified, updateValue
             />
             <div className="flex gap-2">
               <button 
-              className="z-10 pl-1 pr-1 bg-[#3e065f] rounded-2xl hover:scale-[1.1] hover:text-[green] transition-[5]"
+              className="z-10 pl-1 pr-1 bg-[#3e065f] rounded-2xl sm:hover:scale-[1.1] sm:hover:text-[green] transition-[5]"
               onClick={(e) => {
                   e.stopPropagation();
                   e.preventDefault();
@@ -46,7 +46,7 @@ function Card({ item, onDelete, onUpdate, isModified, setIsModified, updateValue
                   }}><FaCheck className="text-[1.9rem] font-bold p-2" />
               </button>
               <button 
-                className="z-10 pl-1 pr-1 bg-[#3e065f] rounded-2xl hover:scale-[1.1] hover:text-[red] transition-[5]"
+                className="z-10 pl-1 pr-1 bg-[#3e065f] rounded-2xl sm:hover:scale-[1.1] sm:hover:text-[red] transition-[5]"
                 onClick={(e) => {
                   e.stopPropagation();
                   e.preventDefault();
@@ -64,7 +64,7 @@ function Card({ item, onDelete, onUpdate, isModified, setIsModified, updateValue
         isModified !== item.id && 
         <div className="flex gap-2">
         <button
-          className="z-10 pl-1 pr-1 bg-[#3e065f] rounded-2xl hover:scale-[1.1] hover:text-[yellow] transition-[5]"
+          className="z-10 pl-1 pr-1 bg-[#3e065f] rounded-2xl sm:hover:scale-[1.1] sm:hover:text-[yellow] transition-[5]"
           onClick={(e) => {
             e.stopPropagation();
             e.preventDefault();
@@ -75,7 +75,7 @@ function Card({ item, onDelete, onUpdate, isModified, setIsModified, updateValue
           <FaPencil className="text-[1.9rem] font-bold p-2" />
         </button>
         <button
-          className="z-10 pl-1 pr-1 bg-[#3e065f] rounded-2xl hover:scale-[1.1] hover:text-[red] transition-[5]"
+          className="z-10 pl-1 pr-1 bg-[#3e065f] rounded-2xl sm:hover:scale-[1.1] sm:hover:text-[red] transition-[5]"
           onClick={(e) => {
             e.stopPropagation();
             e.preventDefault();
