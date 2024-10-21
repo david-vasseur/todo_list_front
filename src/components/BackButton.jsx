6e8817888536d22b138a70cbbf9563d4677d7368
David <p5y4@laposte.net>
2024-10-21 12:28:08 +0200

    testing a hack to correct position fixed not working in mobile

--- src/components/BackButton.jsx
+++ src/components/BackButton.jsx
@@ -16,7 +16,7 @@ function BackButton({ onClick }) {
 
     return (
         <div>
-            <button onClick={handleClick} className="flex items-center text-[#dbd8e3] underline text-[1rem] font-bold hover:scale-[1.15] transition-all duration-50 fixed left-2 sm:left-10">
+            <button onClick={handleClick} className="flex items-center text-[#dbd8e3] underline text-[1rem] font-bold hover:scale-[1.15] transition-all duration-50 fixed left-1 top-20 sm:left-10 sm:top-28">
               <TfiAngleLeft className="mr-1" /> Retour
             </button>
         </div>
