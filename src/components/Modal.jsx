0675071e564cd4bb479612f003fe3eddf99af75e
David <p5y4@laposte.net>
2024-10-21 14:55:54 +0200

    changing modal appearance

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -7,8 +7,8 @@ function Modal() {
         <div>
             {
                 state.isVisible &&
-                    <section className="absolute h-[100vh] w-[100vw] backdrop-blur-md flex flex-col items-center justify-center"> 
-                        <div className="flex flex-col items-center bg-[#8e05c2] opacity-[80%] w-[80vw] sm:w-[30vw] rounded-3xl pt-8 pb-8 p-2 gap-10 border-gray-800 border-2 z-30">
+                    <section className="absolute h-[100vh] w-[100vw] backdrop-blur-md flex flex-col items-center justify-center z-30"> 
+                        <div className="flex flex-col items-center bg-[#8e05c2] opacity-[80%] w-[80vw] sm:w-[30vw] rounded-3xl pt-8 pb-8 p-2 gap-10 border-gray-800 border-2 z-40">
                             <h2>{state.title}</h2>
                             <p className="text-justify text-[#dbd8e3] font-extrabold">{state.message}</p>
                             <button className="bg-[#3e065f] pl-8 pr-8 p-2 rounded-3xl border-gray-800 border-2 text-[#dbd8e3] font-extrabold" onClick={() => {hideModal()}}>OK</button>
